179c461f1fb197e6ace7e362b56f014fdf3bf457
akihironemoto <akihiro.nemoto.06@gmail.com>
2018-04-06 22:30:05 +0900

    汎用viewで書き換え

--- diary/views.py
+++ diary/views.py
@@ -1,71 +1,101 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views import generic
+from django.urls import reverse_lazy
 from .forms import DayCreateForm
 from .models import Day
 
-def index(request):
-	context = {
-		'day_list': Day.objects.all(),
-	}
-	return render(request, 'diary/day_list.html', context)
+# 関数で定義
+# def index(request):
+# 	context = {
+# 		'day_list': Day.objects.all(),
+# 	}
+# 	return render(request, 'diary/day_list.html', context)
+
+# 汎用viewで書き換え
+class IndexView(generic.ListView):
+	model = Day
+
+# 関数で定義
+# def add(request):
+# 	# 送信内容を基にフォームを作る。POSTじゃなければ空のフォーム
+# 	form = DayCreateForm(request.POST or None)
+
+# 	# 入力内容に問題がなければ
+# 	if request.method == 'POST' and form.is_valid():
+# 		form.save()
+# 		return redirect('diary:index')
+
+# 	# 入力内容に誤りがある場合
+# 	context = {
+# 		'form': form
+# 	}
+# 	return render(request, 'diary/day_form.html', context)
+
+# 汎用viewで書き換え
+class AddView(generic.CreateView):
+	model = Day
+	form_class = DayCreateForm
+	success_url = reverse_lazy('diary:index')
 
 
-def add(request):
-	# 送信内容を基にフォームを作る。POSTじゃなければ空のフォーム
-	form = DayCreateForm(request.POST or None)
+# 関数で定義
+# def update(request, pk):
+# 	# urlのpkを基にDayを取得
+# 	day = get_object_or_404(Day, pk=pk)
 
-	# 入力内容に問題がなければ
-	if request.method == 'POST' and form.is_valid():
-		form.save()
-		return redirect('diary:index')
+# 	# フォームに取得したDayを紐付ける
+# 	form = DayCreateForm(request.POST or None, instance=day)
 
-	# 入力内容に誤りがある場合
-	context = {
-		'form': form
-	}
-	return render(request, 'diary/day_form.html', context)
+# 	# 入力に問題がなければ
+# 	if request.method == 'POST' and form.is_valid():
+# 		form.save()
+# 		return redirect('diary:index')
 
+# 	# 入力に誤りがある場合
+# 	context = {
+# 		'form': form
+# 	}
+# 	return render(request, 'diary/day_form.html', context)
 
-def update(request, pk):
-	# urlのpkを基にDayを取得
-	day = get_object_or_404(Day, pk=pk)
-
-	# フォームに取得したDayを紐付ける
-	form = DayCreateForm(request.POST or None, instance=day)
-
-	# 入力に問題がなければ
-	if request.method == 'POST' and form.is_valid():
-		form.save()
-		return redirect('diary:index')
-
-	# 入力に誤りがある場合
-	context = {
-		'form': form
-	}
-	return render(request, 'diary/day_form.html', context)
+# 汎用viewで書き換え
+class UpdateView(generic.UpdateView):
+	model = Day
+	form_class = DayCreateForm
+	success_url = reverse_lazy('diary:index')
 
 
-def delete(request, pk):
-	# urlのpkを基にDayを取得
-	day = get_object_or_404(Day, pk=pk)
+# 関数で定義
+# def delete(request, pk):
+# 	# urlのpkを基にDayを取得
+# 	day = get_object_or_404(Day, pk=pk)
 
-	# 入力に問題がなければ
-	if request.method == 'POST':
-		day.delete()
-		return redirect('diary:index')
+# 	# 入力に問題がなければ
+# 	if request.method == 'POST':
+# 		day.delete()
+# 		return redirect('diary:index')
 
-	# 入力に誤りがある場合
-	context = {
-		'day': day
-	}
-	return render(request, 'diary/day_confirm_delete.html', context)
+# 	# 入力に誤りがある場合
+# 	context = {
+# 		'day': day
+# 	}
+# 	return render(request, 'diary/day_confirm_delete.html', context)
 
+# 汎用viewで書き換え
+class DeleteView(generic.DeleteView):
+	model = Day
+	success_url = reverse_lazy('diary:index')
 
-def detail(request, pk):
-	# urlのpkを基にDayを取得
-	day = get_object_or_404(Day, pk=pk)
+# 関数で定義
+# def detail(request, pk):
+# 	# urlのpkを基にDayを取得
+# 	day = get_object_or_404(Day, pk=pk)
 
-	# 入力に誤りがある場合
-	context = {
-		'day': day
-	}
-	return render(request, 'diary/day_detail.html', context)
+# 	# 入力に誤りがある場合
+# 	context = {
+# 		'day': day
+# 	}
+# 	return render(request, 'diary/day_detail.html', context)
+
+# 汎用viewで書き換え
+class DetailView(generic.DetailView):
+	model = Day
